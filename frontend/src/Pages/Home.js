9699ec1f2f2112a75e3dfcf71615e09b7699652a
Testkil <mctestkil@gmail.com>
2023-02-02 22:39:41 +0100

    the snake was screaming

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -28,7 +28,7 @@ CenteredParagraph.Head = styled.h3`
   font-size: 1.5em;
   text-decoration: underline;
 `;
-CenteredParagraph.paragraph = styled.div`
+CenteredParagraph.Paragraph = styled.div`
   margin: 0;
   border-top: 2px solid;
   padding: 2em 5em;
@@ -37,7 +37,7 @@ CenteredParagraph.paragraph = styled.div`
   }
   border-color: ${(props) => props.theme.colors.accent2};
 `;
-CenteredParagraph.paragraphALT = styled.div`
+CenteredParagraph.ParagraphALT = styled.div`
   margin: 0;
   padding: 2em 5em;
   @media (max-width: 720px) {
@@ -68,40 +68,40 @@ export function Home() {
           <CenteredParagraph.Head style={{ marginBottom: "1em" }}>
             Are you new to TLA or incursions in general?
           </CenteredParagraph.Head>
-          <CenteredParagraph.paragraph>
+          <CenteredParagraph.Paragraph>
             Please have a read of some of the <NavLink to="/guide">GUIDES</NavLink> and join the{" "}
             <a href="https://discord.com/invite/D8pkZhE8DD">DISCORD</a> to ask any questioned that
             aren&apos;t answered here. Look at our <NavLink to="/fits">FITS</NavLink> and make sure
             your fit looks the same and meets the DPS numbers above the fit you choose to fly.
-          </CenteredParagraph.paragraph>
-          <CenteredParagraph.paragraph>
+          </CenteredParagraph.Paragraph>
+          <CenteredParagraph.Paragraph>
             Please join the in-game channel <b>TLA Incursions</b> to X Up for our fleets For any
             questions that aren&apos;t answered below, feel free to drop by and ask! We are more
             than happy to help!
-          </CenteredParagraph.paragraph>
+          </CenteredParagraph.Paragraph>
         </CenteredParagraph>
 
         <ThreeColumn>
           <CenteredParagraph>
             <CenteredParagraph.Head>What Is TLA?</CenteredParagraph.Head>
-            <CenteredParagraph.paragraphALT>
+            <CenteredParagraph.ParagraphALT>
               We are a HQ Incursion Community that specialises in multi-boxing Marauders (Suggested
               fits can be found above). Marauder pilots are guaranteed an additional paid spot in
               fleet for an alt. Your alt is preferred to be in a marauder but can come in anything
               to get payout until its a marauder.
-            </CenteredParagraph.paragraphALT>
+            </CenteredParagraph.ParagraphALT>
           </CenteredParagraph>
           <CenteredParagraph>
             <CenteredParagraph.Head>
               Why are there both Armor and Shield Fits?
             </CenteredParagraph.Head>
-            <CenteredParagraph.paragraphALT>
+            <CenteredParagraph.ParagraphALT>
               We accommodate both tank types to allow anyone to fly their marauder with us.
-            </CenteredParagraph.paragraphALT>
+            </CenteredParagraph.ParagraphALT>
           </CenteredParagraph>
           <CenteredParagraph>
             <CenteredParagraph.Head>FAQ&apos;S</CenteredParagraph.Head>
-            <CenteredParagraph.paragraphALT>
+            <CenteredParagraph.ParagraphALT>
               <b>My fit is different to the one on the website. Can I bring it? </b>
               Fits differing from the ones listed on the website may be welcome in fleet. Ask
               in-game, in Discord or the active FC for clarification.
@@ -110,7 +110,7 @@ export function Home() {
               to fly with TLA. First alts must now hit at least 93% of their respective hull&apos;s
               required DPS. Failure to do will be considered a sponge. (GUNS ONLY, NO HEAT, NO
               DRUGS, NO DRONES)
-            </CenteredParagraph.paragraphALT>
+            </CenteredParagraph.ParagraphALT>
           </CenteredParagraph>
         </ThreeColumn>
       </Content>
