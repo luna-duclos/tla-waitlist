84fe18fb3b98f08a850fe96c29ec28addf0ace87
Ariel Rin <ozirascal@gmail.com>
2024-10-18 06:49:47 +1000

    Add NS/LS Payouts/LP/Overgrid to isk/hr (#36)
    * Add NS/LS Payouts/LP/Overgrid
    
    * correct vg ns

--- frontend/src/Pages/ISKh.js
+++ frontend/src/Pages/ISKh.js
@@ -94,10 +94,14 @@ function decodeData(encoded) {
 function createPayoutTable() {
   var result = {};
   for (const [siteType, payout, lp, normalGrid] of [
-    ["Mothership (highsec)", 63000000, 14000, 80],
-    ["HQ (highsec)", 31500000, 7000, 40],
-    ["AS (highsec)", 18200000, 3500, 20],
-    ["VG (highsec)", 10395000, 1400, 10],
+    ["Mothership (HS)", 63000000, 14000, 80],
+    ["Mothership (LS/NS)", 90000000, 20000, 120],
+    ["HQ (HS)", 31500000, 7000, 40],
+    ["HQ (LS/NS)", 45000000, 10000, 60],
+    ["AS (HS)", 18200000, 3500, 20],
+    ["AS (LS/NS)", 26000000, 5000, 30],
+    ["VG (HS)", 10395000, 1400, 10],
+    ["VG (LS/NS)", 15000000, 2000, 15],
   ]) {
     for (var overgrid = 0; overgrid < 11; overgrid++) {
       const payoutFactor = 1 - 0.0725 * overgrid - 0.0025 * overgrid ** 2;
